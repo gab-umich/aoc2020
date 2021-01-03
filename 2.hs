4b5ecfec0f23dff3ae159061436559c78d9a9cf8
Gabriel Luo <luogm@umich.edu>
2021-01-03 04:15:41 -0500

    2 part 1 done

--- 2.hs
+++ 2.hs
@@ -1,13 +1,32 @@
-import Data.Set as Set
+import qualified Data.Map hiding (map)
+import qualified Data.Set as Set
 
 main = do
-    f <- readFile "1.input"
-    let set = Set.fromList (Prelude.map rInt (lines f))
-    mapM_ (printProdIfMatch set) set
+    f <- readFile "2.input"
+    let tuples = map parseLine (lines f)
+    -- print tuples
+    let result = foldr accu 0 tuples
+    print result
+  where
+    parseLine line = convert (words line)
+      where
+        convert []     = (0, 0, 'a', "a")
+        convert [_]    = (1, 0, 'a', "a")
+        convert [_, _] = (2, 0, 'a', "a")
+        convert [rep, ch, pswd] =
+            ( read (head (split rep)) :: Int
+            , read (last (split rep)) :: Int
+            , head ch
+            , pswd
+            )
+        convert (_ : _ : _ : _ : _) = (4, 0, 'a', "a")
+    accu tup
+        | lower <= occ && occ <= upper = (+ 1)
+        | otherwise                    = (+ 0)
+      where
+        (lower, upper, ch, pswd) = tup
+        occ                      = length $ filter (== ch) pswd
 
-rInt :: String -> Int
-rInt = read
-
-printProdIfMatch collection x
-                            | Set.member (2020 - x) collection = putStrLn $ "found " ++ show x ++ " x " ++ show (2020 - x) ++ " = " ++ show ((2020 - x) * x) ++ ".\n"
-                            | otherwise = return ()
+split s = case dropWhile (== '-') s of
+    "" -> []
+    s' -> w : split s'' where (w, s'') = break (== '-') s'
