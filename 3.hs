d3a053f2bad69bea7a20c87e52f0077cc766faea
Gabriel Luo <luogm@umich.edu>
2021-01-04 20:54:34 -0500

    3 first part done

--- 3.hs
+++ 3.hs
@@ -1,11 +1,11 @@
 import qualified Data.Set as Set
 
 main = do
-    f <- readFile "3-2.test"
-    let result = foldr countTrees (0, 1) (lines f)
+    f <- readFile "3.input"
+    let result = foldl countTrees (0, 0, []) (lines f)
     print result
   where
-    countTrees line (count, index)
-        | (line !! index) == '#' = (count + 1, incre line index)
-        | otherwise              = (count, incre line index)
-        where incre line = mod (length line) . (+ 3)
+    countTrees (count, index, list) line
+        | (line !! index) == '#' = (count + 1, incre line index, list ++ ['#'])
+        | otherwise              = (count, incre line index, list ++ ['.'])
+        where incre line = flip mod (length line) . (+ 3)
